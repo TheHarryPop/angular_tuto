2804031788a97d0c42f4dc81e5a09bdf7be8e6ab
quentin <quentin.winkel@agorinfo.fr>
2025-01-30 10:01:58 +0100

    Cours 2 OC - fin chapitre Observables Nettoyage app.components.ts

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -61,16 +61,16 @@ export class AppComponent implements OnInit {
     }
 
 getTrainObservable$(color: 'rouge' | 'jaune') {
-const isRedTrain = color === 'rouge';
-isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
-const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
-console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
-return of({ color, trainIndex }).pipe(
-delay(isRedTrain ? 5000 : 6000)
-);
-}
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+    delay(isRedTrain ? 5000 : 6000)
+    );
+  }
 
 translateColor(color: 'rouge' | 'jaune') {
-return color === 'rouge' ? 'red' : 'yellow';
-}
+    return color === 'rouge' ? 'red' : 'yellow';
+  }
 }
