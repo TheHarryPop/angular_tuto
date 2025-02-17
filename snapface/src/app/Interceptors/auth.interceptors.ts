7deb71be0720da7b7f6a18f1dbc4e543c3c1b801
quentin <quentin.winkel@agorinfo.fr>
2025-02-17 11:53:26 +0100

    Cours 2 OC

--- snapface/src/app/Interceptors/auth.interceptors.ts
+++ snapface/src/app/Interceptors/auth.interceptors.ts
@@ -9,7 +9,7 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private auth: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const headers = new HttpHeaders().append('Authorization', `Bear ${this.auth.getToken()}`)
+    const headers = new HttpHeaders().append('Authorization', `Bearer ${this.auth.getToken()}`)
     const modifiedReq = req.clone({headers});
     return next.handle(modifiedReq);
   }
